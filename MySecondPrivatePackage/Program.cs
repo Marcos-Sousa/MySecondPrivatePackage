d12ae92befbdcc78b9b7b895eb3ce47be7d4d0d3
Marcos Sousa Lima <marcossousalima6@gmail.com>
2022-07-02 09:35:47 -0300

    iniciando pacote

---
+++ MySecondPrivatePackage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MySecondPrivatePackage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
